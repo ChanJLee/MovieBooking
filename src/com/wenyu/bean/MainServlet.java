f5de320fdc09463883fe6b714a826e95b781a61f
wenyu <1355425625@qq.com>
2017-03-12 10:44:06 +0800

    add packge

---
+++ src/com/wenyu/bean/MainServlet.java
@@ -0,0 +1,16 @@
+package com.wenyu.bean;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by admin on 2017/3/12.
+ */
+public class MainServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
